158ee9ed7a0e3582d416cde04a527b75bb99c7fd
Ignotas Sulzenko S1137931 <s1137931@sms.ed.ac.uk>
2013-10-19 21:47:11 +0100

    Added a Bus class that describes a bus on the bus network

--- Models.py
+++ Models.py
@@ -2,8 +2,18 @@
 This file contains class descriptions for all kinds of objects
 used in the simulation to mimic the real world: stops, roads, passengers and etc.
 '''
-
 class Passenger:
     ''' A class representing a passenger in bus network'''
     def __init__(self, destStopID):
         self.destStopID = destStopID
+        
+        
+class Bus:
+    ''' A class representing a bus going on some route in the bus network'''
+    def __init__(self, routeID, busNumber, capacity, status, location):
+        self.routeID = routeID
+        self.busNumber = busNumber
+        self.capacity = capacity
+        self.status = 'Queueing'
+        self.location = location
+        self.passengers = []
