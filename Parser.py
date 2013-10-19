85fc596c0ad4863aa80c2b51163d801c2dcbb587
Ignotas Sulzenko S1137931 <s1137931@sms.ed.ac.uk>
2013-10-19 22:44:39 +0100

    Adding a blank Parser.py file that will (in future) define the input parser

---
+++ Parser.py
@@ -0,0 +1,5 @@
+'''
+A file containing Parser class that parses the given input. It translates input 
+lines into method calls to construct the model of bus network and set the 
+parameters of the simulation.
+'''
