a0a683dca82512ec880217304bcbaed593e231ab
Ignotas Sulzenko S1137931 <s1137931@sms.ed.ac.uk>
2013-10-20 00:13:40 +0100

    Added a static parseLine method that parses the input and invokes appropriate Network and Simulation object methods

--- Parser.py
+++ Parser.py
@@ -19,3 +19,32 @@ class Parser:
         for line in inputFile:
             parseLine(line.strip(), network, simulation)
         return network, simulation
+
+
+    @staticmethod
+    def __parseLine(line, network, simulation):
+        ''' Method for parsing a line of input into a method call that changes
+        the network and simulation objects'''
+        # Parsing arguments that affect the simulation object:
+        if line.startswith('board'):
+            simulation.setBoardRatio(line.split(' ')[1])
+        elif line.startswith('disembarks'):
+            simulation.setDisembarksRatio(line.split(' ')[1])
+        elif line.startswith('departs'):
+            simulation.setDepRatio(line.split(' ')[1])
+        elif line.startswith('new passengers'):
+            simulation.setNewPassRatio(line.split(' ')[2])
+        elif line.startswith('stop time'):
+            simulation.setStopTime(line.split(' ')[2])
+        elif line.startswith('ignore warnings'):
+            simulation.setIgnoreWarn()
+        elif line.startswith('optimise parameters'):
+            simulation.optimiseParams()
+        # Parsing arguments that affect the network object:
+        elif line.startswith('route'):
+            matches = re.search('''route\s([0-9]*)\sstops\s([0-9 ]*)\sbuses\s
+                                ([0-9]*)\scapacity\s([0-9]*)''', line)
+            network.addRoute(matches[0], matches[1], matches[2], matches[3])
+        elif line.startswith('road'):
+            args = line.split(' ')
+            network.addRoad(args[1], args[2], args[3])
