11bb84cf1d19fbeba7c7799118e513b9b86cfb30
Ignotas Sulzenko S1137931 <s1137931@sms.ed.ac.uk>
2013-10-20 00:12:01 +0100

    Added strip() to strip the whitespace when parsing the input

--- Parser.py
+++ Parser.py
@@ -3,6 +3,7 @@ A file containing Parser class that parses the given input. It translates input
 lines into method calls to construct the model of bus network and set the 
 parameters of the simulation.
 '''
+import re
 from Models import Network
 
 class Parser:
@@ -16,5 +17,5 @@ class Parser:
         network = Network
         simulation = Simulation
         for line in inputFile:
-            parseLine(line, network, simulation)
+            parseLine(line.strip(), network, simulation)
         return network, simulation
