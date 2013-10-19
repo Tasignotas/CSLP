b095cbc9add23fdb71c910ef2ee1bd9d92d47516
Ignotas Sulzenko S1137931 <s1137931@sms.ed.ac.uk>
2013-10-20 00:34:34 +0100

    Added a method that adds a road to the network

--- Models.py
+++ Models.py
@@ -47,3 +47,11 @@ class Network:
         self.routes = {}
         self.stops = {}
         self.roads = Roads
+    
+    
+    def addRoad(self, stop1, stop2, throughput):
+        ''' This method adds a road with specified throughput between two stops,
+        stop1 and stop2'''
+        if not (stop1 in self.roads.keys()):
+            self.roads[stop1] = {}
+        self.roads[stop1][stop2] = throughput
