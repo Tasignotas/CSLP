c4dfb218260fef3b9340f99c04a784ba8c96d862
Ignotas Sulzenko S1137931 <s1137931@sms.ed.ac.uk>
2013-10-20 01:52:46 +0100

    Adding the constructor of the Simulation class

--- Simulation.py
+++ Simulation.py
@@ -2,3 +2,9 @@
 This class will perform events on the constructed bus network: it will simulate
 passenger and bus movement/queueing in the network
 '''
+class Simulation:
+    ''' A class that controls the entire simulation and performs events using
+    the constructed bus network'''
+    def __init__(self):
+        self.ignoreWarnings = False
+        self.optimiseParameters = False
