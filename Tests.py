4be8e7a9dc256ed65ec2a85c81e9b94374526db1
Ignotas Sulzenko S1137931 <s1137931@sms.ed.ac.uk>
2013-12-12 18:07:13 +0000

    Added unit tests for stop time, ignore warnings and optimise parameters

--- Tests.py
+++ Tests.py
@@ -6,6 +6,7 @@ from mock import Mock
 from mock import patch
 
 class ParserTests(unittest.TestCase):
+
     
     def setUp(self):
         self.network = Models.Network()
@@ -18,6 +19,7 @@ class ParserTests(unittest.TestCase):
         self.assertEqual(self.simulation.boardRatioList, [0.5, 0.6, 0.7])
         Parser.Parser._parseLine('board 0.5', self.network, self.simulation)
         self.assertEqual(self.simulation.boardRatioList, [0.5])
+        self.assertRaises(Exception, Parser.Parser._parseLine, 'board ', self.network, self.simulation)
         self.assertRaises(Exception, Parser.Parser._parseLine, 'board 0.5 0.6 0.7', self.network, self.simulation)
         
     def testDisembarks(self):
@@ -46,7 +48,25 @@ class ParserTests(unittest.TestCase):
         self.assertEqual(self.simulation.newPassRatioList, [0.5])
         self.assertRaises(Exception, Parser.Parser._parseLine, 'new passengers 0.5 0.6 0.7', self.network, self.simulation)
         
+    
+    def testStopTime(self):
+        ''' Tests if the stop time parameters are parsed correctly by the parser'''
+        Parser.Parser._parseLine('stop time 111.1', self.network, self.simulation)
+        self.assertEqual(self.simulation.stopTime, 111.1)
         
+    
+    def testIgnoreWarnings(self):
+        ''' Tests if the ignore warnings flag was set as expected'''
+        Parser.Parser._parseLine('ignore warnings', self.network, self.simulation)
+        self.assertEqual(self.simulation.ignoreWarnings, True)
+
+
+    def testOptimiseParameters(self):
+        ''' Tests if the ignore warnings flag was set as expected'''
+        Parser.Parser._parseLine('optimise parameters', self.network, self.simulation)
+        self.assertEqual(self.simulation.optimiseParameters, True)
+
+
     def testRoad(self):
         ''' Tests if the road parameters are parsed correctly by the parser:
             1. Checks if the addRoad method is called with correct parameters
