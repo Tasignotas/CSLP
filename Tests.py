54dcde93262577e11d30f545dd6e1ac7e52f8c6d
Ignotas Sulzenko S1137931 <s1137931@sms.ed.ac.uk>
2013-12-12 16:11:51 +0000

    Added unit tests for the Parser, particularly the main _parseLine method

---
+++ Tests.py
@@ -0,0 +1,69 @@
+import Parser
+import Models
+import Simulation
+import unittest
+from mock import Mock
+from mock import patch
+
+class ParserTests(unittest.TestCase):
+    
+    def setUp(self):
+        self.network = Models.Network()
+        self.simulation = Simulation.Simulation()
+    
+    
+    def testBoard(self):
+        ''' Tests if the board parameters are parsed correctly by the parser'''
+        Parser.Parser._parseLine('board experiment 0.5 0.6 0.7', self.network, self.simulation)
+        self.assertEqual(self.simulation.boardRatioList, [0.5, 0.6, 0.7])
+        Parser.Parser._parseLine('board 0.5', self.network, self.simulation)
+        self.assertEqual(self.simulation.boardRatioList, [0.5])
+        self.assertRaises(Exception, Parser.Parser._parseLine, 'board 0.5 0.6 0.7', self.network, self.simulation)
+        
+    def testDisembarks(self):
+        ''' Tests if the disembarks parameters are parsed correctly by the parser'''
+        Parser.Parser._parseLine('disembarks experiment 0.5 0.6 0.7', self.network, self.simulation)
+        self.assertEqual(self.simulation.disembarksRatioList, [0.5, 0.6, 0.7])
+        Parser.Parser._parseLine('disembarks 0.5', self.network, self.simulation)
+        self.assertEqual(self.simulation.disembarksRatioList, [0.5])
+        self.assertRaises(Exception, Parser.Parser._parseLine, 'disembarks 0.5 0.6 0.7', self.network, self.simulation)
+        
+        
+    def testDeparts(self):
+        ''' Tests if the departs parameters are parsed correctly by the parser'''
+        Parser.Parser._parseLine('departs experiment 0.5 0.6 0.7', self.network, self.simulation)
+        self.assertEqual(self.simulation.depRatioList, [0.5, 0.6, 0.7])
+        Parser.Parser._parseLine('departs 0.5', self.network, self.simulation)
+        self.assertEqual(self.simulation.depRatioList, [0.5])
+        self.assertRaises(Exception, Parser.Parser._parseLine, 'departs 0.5 0.6 0.7', self.network, self.simulation)
+        
+        
+    def testNewPassengers(self):
+        ''' Tests if the new passengers parameters are parsed correctly by the parser'''
+        Parser.Parser._parseLine('new passengers experiment 0.5 0.6 0.7', self.network, self.simulation)
+        self.assertEqual(self.simulation.newPassRatioList, [0.5, 0.6, 0.7])
+        Parser.Parser._parseLine('new passengers 0.5', self.network, self.simulation)
+        self.assertEqual(self.simulation.newPassRatioList, [0.5])
+        self.assertRaises(Exception, Parser.Parser._parseLine, 'new passengers 0.5 0.6 0.7', self.network, self.simulation)
+        
+        
+    def testRoad(self):
+        ''' Tests if the road parameters are parsed correctly by the parser:
+            1. Checks if the addRoad method is called with correct parameters
+            2. Checks if an exception is raised with incorrectly structured input'''
+        with patch.object(self.network, 'addRoad') as mock:
+            Parser.Parser._parseLine('road 1 2 0.3', self.network, self.simulation)
+        mock.assert_called_with(1, 2, 0.3)
+        
+        
+    def testRoute(self):
+        ''' Tests if the route parameters are parsed correctly by the parser:
+            1. Checks if the addRoute method is called with correct parameters
+            2. Checks if an exception is raised with incorrectly structured input'''
+        with patch.object(self.network, 'addRoute') as mock:
+            Parser.Parser._parseLine('route 1 stops 1 2 3 buses 4 capacity 50', self.network, self.simulation)
+        mock.assert_called_with(1, [1, 2, 3], 4, 50)
+
+
+if __name__ == '__main__':
+    unittest.main()
